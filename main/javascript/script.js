c6f2e095dc3d60a42025a01ad63630ceb22edfbf
Jayden-Franklin <jay.franklin-tomlins@ada.ac.uk>
2025-09-03 16:44:04 +0100

    Nav Bars

--- main/javascript/script.js
+++ main/javascript/script.js
@@ -7,3 +7,11 @@ function jumpToSection() {
         }
     });
 }
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
